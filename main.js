a47d12c19ecec42e06b2dc2b6e81208c23b94a39
Tomislav Dukez <tomdu3@ymail.com>
2024-05-20 21:32:46 +0200

    add: collision detection function

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ class GameScene extends Phaser.Scene {
     this.cursor;
     this.playerSpeed = speedDown + 50;
     this.target;
+
+    // collision
+    this.points = 0;
   }
   preload() {
     this.load.image("bg", "./assets/bg.png");
@@ -55,6 +58,13 @@ class GameScene extends Phaser.Scene {
 
   getRandomX() {
     return Math.floor(Math.random() * 480);
+  }
+
+  // collision detection
+  targetHit() {
+    this.target.setY(0);
+    this.target.setX(this.getRandomX());
+    this.points++;
   }
 }
 
