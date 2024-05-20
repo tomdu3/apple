33342a18bfc1b0d74e35fac546407c5f9ee62966
Tomislav Dukez <tomdu3@ymail.com>
2024-05-20 19:39:53 +0200

    add: physics and debug true

--- main.js
+++ main.js
@@ -6,11 +6,22 @@ const sizes = {
   height: 500,
 };
 
+const speedDown = 300;
+
 const config = {
   type: Phaser.WEBGL,
   width: sizes.width,
   height: sizes.height,
   canvas: gameCanvas,
+  // physics properties
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: speedDown },
+      debug: true,
+    },
+  },
+  scene: [GameScene],
 };
 
 const game = new Phaser.Game(config);
