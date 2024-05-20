25e8baff3e90551df8376eba3ecd367f74f80f1b
Tomislav Dukez <tomdu3@ymail.com>
2024-05-20 20:12:22 +0200

    add: move control to player

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ class GameScene extends Phaser.Scene {
   constructor() {
     super("scene-game");
     this.player;
+    this.cursor;
+    this.playerSpeed = speedDown + 50;
   }
   preload() {
     this.load.image("bg", "./assets/bg.png");
@@ -22,8 +24,23 @@ class GameScene extends Phaser.Scene {
     this.player = this.physics.add
       .image(0, sizes.height - 100, "basket")
       .setOrigin(0, 0);
+    this.player.setImmovable(true); // player immovable
+    this.player.body.allowGravity = false; // player not affected by gravity
+    this.player.setCollideWorldBounds(true); // player does not go out of bounds
+
+    this.cursor = this.input.keyboard.createCursorKeys();
   } // creating game objects
-  update() {} // updating game objects continuously
+  update() {
+    const { left, right } = this.cursor;
+
+    if (left.isDown) {
+      this.player.setVelocityX(-this.playerSpeed);
+    } else if (right.isDown) {
+      this.player.setVelocityX(this.playerSpeed);
+    } else {
+      this.player.setVelocityX(0);
+    }
+  } // updating game objects continuously
 }
 
 const config = {
