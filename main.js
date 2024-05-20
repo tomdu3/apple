62b823d722ed3ffc04cc00e0851e10ec6c5f24a6
Tomislav Dukez <tomdu3@ymail.com>
2024-05-20 19:37:27 +0200

    Add: default canvas app

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 import "./style.css";
+import Phaser from "phaser";
+
+const sizes = {
+  width: 500,
+  height: 500,
+};
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+};
+
+const game = new Phaser.Game(config);
